841294f37a1c478e246d548d045b48ca5a8bc3f3
Ryan <blhack@gmail.com>
2013-01-15 20:26:39 +0000

    Door status is now working

--- js/door_status.js
+++ js/door_status.js
@@ -1,10 +1,21 @@
 require([
-	"dojo/request"
-], function(request){
+	"dojo/request",
+	"dojo/dom"
+], function(request,dom){
+	
+	//set the door status to something while we query space API
+	
+	dom.byId("door_status").innerHTML = "<i>fetching door status</i>";
+
  	request("http://intranet.heatsynclabs.org/~access/cgi-bin/spaceapi.rb", 
 	{handleAs: "json", headers: {"X-Requested-With":null}}
 	).then(function(data){
-		console.log(data["open"]);
+		if (data["open"] == false) {
+			dom.byId("door_status").innerHTML = "We are currently closed";
+			}
+		if (data["open"] == true) {
+			dom.byId("door_status").innerHTML = "Come in, we're open!";
+			}
   	}, function(err){
 		console.log("Did not get door data.");
   	}, function(evt){
