33ff51ba4bc2118a8fa9b96311db4602133f4f1b
Ryan <blhack@gmail.com>
2013-01-21 22:48:45 +0000

    Made flickr api client results more generic.  Next up is making it globally available.

--- js/flickr.js
+++ js/flickr.js
@@ -2,8 +2,9 @@ define([
   	'dojo/dom',
 	'dojo/request'
 ], function(dom,request){
-	var photos = [];
 	console.log("Flickr init...");
+
+	var photos = [];
 
 	var url = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=bec64c9c0f28889dc6e0c5ef7be3511f&user_id=60827818%40N07&tags=publish&format=json&nojsoncallback=1';
 
@@ -18,10 +19,11 @@ define([
 
 			photo = data.photos.photo[i];
 
-			url = "http://farm" + photo['farm'] + ".staticflickr.com/" + photo["server"] + "/" + photo["id"] + "_" + photo["secret"] + "_b.jpg"
+			image_url = "http://farm" + photo['farm'] + ".staticflickr.com/" + photo["server"] + "/" + photo["id"] + "_" + photo["secret"] + "_b.jpg"
 			title = photo['title'];
-
-			photos[i] = [title,url];
+			link = "http://www.flickr.com/photos/hslphotosync/" + photo["id"] + "/in/photostream";
+			description = "";
+			photos[i] = {"title":title,"description":description,"image_url":image_url,"link":link};
 		}
 		console.log(photos);
 	});
