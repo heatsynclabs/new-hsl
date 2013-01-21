1a0b59a61d38d7873aaeb2b7bd9c9066913b76b2
Blaine Bublitz <blaine@iceddev.com>
2013-01-21 02:25:34 -0700

    cleanup door status code

--- js/door_status.js
+++ js/door_status.js
@@ -1,24 +1,31 @@
 require([
-	"dojo/request",
-	"dojo/dom"
-], function(request,dom){
-	
-	//set the door status to something while we query space API
-	
-	//dom.byId("door_status").innerHTML = "<i>fetching door status</i>";
+  'dojo/request',
+  'dojo/dom',
+  'dojo/domReady!'
+], function(request, dom){
 
- 	request("http://intranet.heatsynclabs.org/~access/cgi-bin/spaceapi.rb", 
-	{handleAs: "json", headers: {"X-Requested-With":null}}
-	).then(function(data){
-		if (data["open"] == false) {
-				dom.byId("door_status").innerHTML = '<div class="signs closed"></div>';                
-			}
-		if (data["open"] == true) {
-				dom.byId("door_status").innerHTML = '<div class="signs open"></div>';
-			}
-  	}, function(err){
-		console.log("Did not get door data.");
-  	}, function(evt){
-	// handle a progress event
-	});
+  //set the door status to something while we query space API
+  'use strict';
+
+  var url = 'http://intranet.heatsynclabs.org/~access/cgi-bin/spaceapi.rb';
+
+  var doorStatus = dom.byId('door_status');
+
+  request.get(url, {
+    handleAs: 'json',
+    headers: {
+      'X-Requested-With': null
+    }
+  }).then(function(data){
+    if(!data.open){
+      doorStatus.innerHTML = '<div class="signs closed"></div>';
+    }
+    if(data.open){
+      doorStatus.innerHTML = '<div class="signs open"></div>';
+    }
+  }, function(err){
+    console.log('Did not get door data.', err);
+    doorStatus.innerHTML = 'Error occurred while fetching status, please refresh.';
+  });
+
 });
