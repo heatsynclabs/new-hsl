a576e8cf941292060e3aed7f4866cce9b672862f
Blaine Bublitz <blaine@iceddev.com>
2013-10-31 02:04:34 -0700

    update grunt stuff

--- js/replaceTags.js
+++ js/replaceTags.js
@@ -1,7 +1,9 @@
 define(function(){
 
   return function(str){
-    if (str == null) return '';
+    if (str == null){
+      return '';
+    }
     return String(str).replace(/<\/?[^>]+>/g, ' ');
   };
 
