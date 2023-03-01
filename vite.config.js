8d317e32c98de96eda434dc4c3a06bc84b43d865
Mandy <amanda.everett.codes@gmail.com>
2023-03-01 16:21:41 -0700

    chore: update linting ignore and config base, rename test rect component naming
    * basic setup for vite with react
    
    * feat: folder structure and vite config support existing static routes
    
    * revert relative pathh from absolute
    
    * style: updated react component naming to be more semantic
    
    * chore: added .eslintignore and added airbnb-base back to eslintconfig
    
    * fix: remove leftover merge conflict
    
    ---------
    
    Co-authored-by: Mike Wolfson <mwolfson@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react";
-import forwardToTrailingSlashPlugin from "./forward-to-trailing-slash-plugin.js";
+import forwardToTrailingSlashPlugin from "./forward-to-trailing-slash-plugin";
 
 const build = {
   rollupOptions: {
