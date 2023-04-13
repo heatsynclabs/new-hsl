1028581e59822946a1cf4b69c434815c4fc65ebf
ismandycoding <amanda.everett.codes@gmail.com>
2023-04-12 20:01:22 -0700

    feat: added not found specific error handling on ErrorPage

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -3,7 +3,6 @@ import NavBar from "./NavBar";
 
 export default function ErrorPage() {
   const error = useRouteError();
-  console.error(error);
 
   return (
     <>
@@ -12,7 +11,25 @@ export default function ErrorPage() {
         <h1>Oops!</h1>
         <p>Sorry, an unexpected error has occurred.</p>
         <p>
-          <i>{error.statusText || error.message}</i>
+          <i>
+            {error.status === 404
+              ? "The page you tried to reach was not found."
+              : null}
+          </i>
+        </p>
+        <p>
+          <i>
+            {" "}
+            If the error persists, please reach out in the{" "}
+            <a
+              href="https://groups.google.com/forum/?fromgroups#!forum/heatsynclabs"
+              alt="HSL Google Group"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Discussion Group
+            </a>{" "}
+          </i>
         </p>
       </div>
     </>
