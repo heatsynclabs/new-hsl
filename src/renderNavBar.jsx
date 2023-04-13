1a1e709885dac12fed39b9bbf9a7a677aef3d474
ismandycoding <amanda.everett.codes@gmail.com>
2023-04-12 20:31:51 -0700

    feat: convert homepage and navbar to react components

---
+++ src/renderNavBar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import NavBar from "./shared/NavBar";
+
+ReactDOM.createRoot(document.getElementById("navBar")).render(
+  <React.StrictMode>
+    <NavBar />
+  </React.StrictMode>
+);
