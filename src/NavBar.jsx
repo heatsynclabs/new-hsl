16a6d99fa397d526cebb2d2e00b36ad244584dec
ismandycoding <amanda.everett.codes@gmail.com>
2023-04-17 18:49:10 -0700

    fix: logo image path in navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -6,7 +6,7 @@ export default function NavBar() {
       <div className="logo">
         <a href="/">
           <img
-            src="img/heatsync-labs-logo__white.png"
+            src="./heatsync-labs-logo__white.png"
             title="HeatSync Labs"
             alt="HeatSync Labs"
           />
